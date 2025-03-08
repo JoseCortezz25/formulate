520c873e564422f55a74a0db31ccdac8246baa2d
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2025-03-08 16:52:51 -0500

    Add Hero component and blue-mark styling; update Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,15 @@
+import Hero from "@/components/home/hero";
 import StepsSection from "@/components/home/steps-sections";
 import { Github } from "@/components/icons";
 import Link from "next/link";
 
 export default function Home() {
+
   return (
     <main>
       {/* Hero Section */}
-      <section className="w-full pt-[144px] pb-[144px] h-screen flex items-center px-6">
-        <div className="w-full mx-auto lg:max-w-[980px] flex lg:justify-center flex-col lg:items-center relative">
-          <div className="w-full mb-8">
-            <h1 className="text-fm-black text-center text-[25px] md:text-[38.75px] font-bold leading-[44px] mb-[8px]">
-              With Formulate
-            </h1>
-
-            <h2 className="text-fm-black text-center font-bold text-[40px] leading-[46px] lg:text-[82px] lg:leading-[82px] mb-[20px]">
-              Create forms.<br />
-              Save time.
-            </h2>
-
-            <p className="text-center justify-center text-[#6e6e73] text-lg leading-snug lg:text-2xl lg:leading-8 font-semibold tracking-tight">
-              Generate forms for React (with Shadcn + React Hook Form) or native HTML (with Just Validate), ready for production, without the headache.
-            </p>
-          </div>
-
-          <div className="text-center flex space-x-2 mt-[20px]">
-            <button className="border-2 border-fm-black py-3 px-6 rounded-full font-semibold hover:bg-fm-black hover:text-white transition-colors duration-200 ease-in-out cursor-pointer">
-              <Link href="builder">
-                Get Started
-              </Link>
-            </button>
-
-            <button className="bg-fm-black border-2 border-fm-black p-3 rounded-full font-semibold cursor-pointer">
-              <a href="https://github.com/JoseCortezz25/formulate" target="_blank" rel="noopener noreferrer">
-                <Github className="text-white size-6" />
-              </a>
-            </button>
-          </div>
-
-        </div>
-      </section>
-
-      {/* Icons */}
+      <Hero />
+      {/* Icons Sections */}
       <StepsSection />
 
       {/* Footer */}
@@ -48,8 +17,8 @@ export default function Home() {
         <div className="mx-auto lg:max-w-[980px] flex flex-col md:flex-row justify-between items-center">
           <div className="lg:max-w-[50%]">
             <h2 className="text-fm-black text-[25px] leading-[30px] font-bold mb-[20px] text-center md:text-left">
-              Simple, sencillo y rápido. <br />
-              Exporta tu formulario en segundos.
+              Simple, fast and easy. <br />
+              Create forms in seconds.
             </h2>
           </div>
 
