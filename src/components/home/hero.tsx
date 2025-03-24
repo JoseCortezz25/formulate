b729c2afcd809414f81023370c5fa1c5a3e0f1e4
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2025-03-24 14:28:39 -0500

    Refactor Hero component: remove motion animations, simplify button structure, and enhance accessibility features

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,26 +1,9 @@
 "use client";
 
-import { motion, Variants } from "motion/react";
 import Link from "next/link";
 import { Github } from "../icons";
 
 const Hero = () => {
-  const showImage: Variants = {
-    offscreen: {
-      scale: 0.8,
-      opacity: 0,
-      y: 50
-    },
-    onscreen: {
-      opacity: 1,
-      y: 0,
-      scale: 1.2,
-      transition: {
-        type: "spring"
-      }
-    }
-  };
-
   return (
     <section className="w-full pt-[144px] pb-[50px] md:pb-[144px] min-h-screen flex items-center px-6">
       <div className="w-full mx-auto lg:max-w-[980px] flex lg:justify-center flex-col lg:items-center relative">
@@ -40,31 +23,21 @@ const Hero = () => {
         </div>
 
         <div className="text-center mx-auto flex space-x-2 mt-[20px]">
-          <button className="z-10 border-2 border-fm-black py-3 px-6 rounded-full font-semibold hover:bg-fm-black hover:text-white transition-colors duration-200 ease-in-out cursor-pointer">
-            <Link href="builder">
-              Get Started
-            </Link>
-          </button>
+          <Link href="builder">
+            <button className="z-10 border-2 border-fm-black py-3 px-6 rounded-full font-semibold hover:bg-fm-black hover:text-white transition-colors duration-200 ease-in-out cursor-pointer">
+                Get Started
+            </button>
+          </Link>
 
-          <button className="bg-fm-black border-2 border-fm-black p-3 rounded-full font-semibold cursor-pointer">
-            <a href="https://github.com/JoseCortezz25/formulate" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/JoseCortezz25/formulate" target="_blank" rel="noopener noreferrer">
+            <button className="bg-fm-black border-2 border-fm-black p-3 rounded-full font-semibold cursor-pointer">
               <Github className="text-white size-6" />
-            </a>
-          </button>
+            </button>
+          </a>
         </div>
-
-        <motion.div
-          initial="offscreen"
-          whileInView="onscreen"
-          viewport={{ amount: 0.75, once: true }}
-        >
-          <motion.div
-            className="hidden md:flex"
-            variants={showImage}
-          >
-            <img src="/screenshot.png" alt="Screenshot of page" />
-          </motion.div>
-        </motion.div>
+        <div className="hidden md:flex">
+          <img src="/screenshot.png" alt="Screenshot of page" />
+        </div>
       </div>
     </section>
   );
